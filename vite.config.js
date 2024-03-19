bcb1a3ddf4924802cad69d2215d0f048606bbe14
Viktoravanesov <el.eng1989@rambler.ru>
2024-03-19 20:17:37 +0300

    my changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/weather-project-geo",
+  base : "/Weather-project-geo",
   plugins: [react()],
 })
